79080350fb8ec25e359e2683ea05359669f9dea9
maciej790 <maciejmw126@gmail.com>
2021-12-27 20:30:25 +0100

    done header

---
+++ index.js
@@ -0,0 +1,7 @@
+//testing scale animation
+// const btn = document.querySelector('.header__toggleCurrency');
+// const element = document.querySelector('.header__currency');
+
+// btn.addEventListener('click', () =>{
+//     element.classList.toggle('scale');
+// })
